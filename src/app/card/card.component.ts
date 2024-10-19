2af61ff2c41f7721f0097a0d5f691888ec65960c
Sam Graber <authorsamgraber@gmail.com>
2024-10-19 14:53:26 -0400

    Show card states
    The card component should reflect the state of the card and display accordingly. We can use the new built in control flow to switch the UI based on the card state
    
    I added a temporary state switcher method on click for demonstrative purposes. This will work a little differently once the game is implemented since it needs to trigger the game to run.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import Card from './card.model';
+import Card, { CardState } from './card.model';
 
 @Component({
   selector: 'app-card',
@@ -10,4 +10,21 @@ import Card from './card.model';
 })
 export class CardComponent {
   @Input() card?: Card;
+  CardState = CardState;
+
+  cardClicked() {
+    if (this.card) {
+      switch(this.card.state) {
+        case CardState.hidden:
+          this.card.state = CardState.revealed;
+          break;
+        case CardState.revealed:
+          this.card.state = CardState.removed;
+          break;
+        default:
+          this.card.state = CardState.hidden;
+          break;
+      }
+    }
+  }
 }
