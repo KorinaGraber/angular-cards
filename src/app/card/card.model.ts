895db20860361bfa19e0e98a88893e4c5384608b
Sam Graber <authorsamgraber@gmail.com>
2024-10-19 16:43:07 -0400

    Add metadata for cards
    We need to know the numerical value of cards to compare for matches. The suit is additional data that would be useful for other games if I end up continuing the project

--- src/app/card/card.model.ts
+++ src/app/card/card.model.ts
@@ -8,12 +8,16 @@ export interface RawCard {
   id: number;
   name: string;
   image: string;
+  suit: string;
+  value: number;
 }
 
 export default class Card {
   id: number = 0;
   name: string = 'Sample card';
   image: string = '';
+  suit: string = '';
+  value: number = 0;
   state: CardState = CardState.hidden;
 
   constructor(rawData?: RawCard) {
@@ -21,6 +25,8 @@ export default class Card {
       this.id = rawData.id;
       this.name = rawData.name;
       this.image = rawData.image;
+      this.suit = rawData.suit;
+      this.value = rawData.value;
     }
   }
 }
